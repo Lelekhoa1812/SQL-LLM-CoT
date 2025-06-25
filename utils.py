feac83b607ec1ea55409ea153753b9cae4225d99
Khoa Le <binkhoa1812@gmail.com>
2025-06-25 20:56:57 +0700

    Upd utils

--- utils.py
+++ utils.py
@@ -3,11 +3,11 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # DB connection
-user = os.getenv("DB_USER", "sql12786766")
-pw = os.getenv("DB_PASSWORD", "3WUxNFxSyc")
-host = os.getenv("DB_HOST", "sql12.freesqldatabase.com")
+user = os.getenv("DB_USER")
+pw = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
 port = os.getenv("DB_PORT", "3306")
-db = os.getenv("DB_NAME", "sql12786766")
+db = os.getenv("DB_NAME")
 engine = create_engine(f"mysql+pymysql://{user}:{pw}@{host}:{port}/{db}")
 
 # Execute and fetch
